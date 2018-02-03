8c5dd106ba4313dbe205c527fdee4396b25de647
Jess <zjess987@gmail.com>
2018-02-02 22:12:52 -0500

    added enemy boxes + camera controller to follow the player

---
+++ Game Dev Project/Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static int health;
+
+    public static int numWool;
+    public static int numFlowers;
+    public static int numCatnip;
+    public static int numGrapes;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Game Dev Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public float Xmax;
+    public float Xmin;
+    //public float Ymax;
+    //public float Ymin;
+
+    public Vector3 offset;
+
+    public Transform player;
+
+    private void Start()
+    {
+        Xmin = -3f;
+        Xmax = 3f;
+        //Ymin = -1.1f;
+        //Ymax = 1.5f;
+           
+    }
+
+    void Update()
+    {
+        if (player.position.x > Xmin && player.position.x < Xmax) // && player.position.y > Ymin && player.position.y < Ymax
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, 0); 
+    }
+}
