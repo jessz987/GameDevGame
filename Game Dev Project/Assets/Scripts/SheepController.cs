2a02e7080f6e6e7d939e8164ebc57d4064f1ccb4
Jess <zjess987@gmail.com>
2018-02-16 15:14:23 -0500

    sheep animation added (buggy)

--- Game Dev Project/Assets/Scripts/SheepController.cs
+++ Game Dev Project/Assets/Scripts/SheepController.cs
@@ -6,15 +6,30 @@ public class SheepController : MonoBehaviour {
     
     public Transform[] patrolPositions;
     public float speed;
-    
+
+    public Animator anim;
+    public string currentAnim;
 
     void Update()
     {
+        anim = GetComponent<Animator>();
+        currentAnim = "SheepLeft";
+        
+
         Patrol();
     }
     
     void Patrol()
     {
         transform.position = Vector3.Lerp(patrolPositions[0].position, patrolPositions[1].position, Mathf.PingPong(Time.time * speed, 1f));
+        if (transform.position.x >= patrolPositions[1].position.x - 0.01f)
+        {
+            anim.SetBool("ToRight", false);
+        }
+
+        if (transform.position.x <= patrolPositions[0].position.x + 0.01f)
+        {
+            anim.SetBool("ToRight", true);
+        }
     }
 }
